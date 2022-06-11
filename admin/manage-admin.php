c3e009ef09e9ad53336626298cf23b16d0b06077
Jhilik Maji <jhilik.maji98@gmail.com>
2022-06-11 20:06:21 +0530

    second commit(admin structure)

---
+++ admin/manage-admin.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Manage Admin</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+
+        <?php include('partials/menu.php'); ?>
+       
+        <!-- main contain section start -->
+        <div class="main-content">
+        <div class="wrapper">
+               <h1>MANAGE ADMIN</h1>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- main contain section end -->
+
+       <?php include('partials/footer.php');?>
+
+
+
+
+
+    </body>
+</html>
---
+++ admin/manage-category.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Manage Category </title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+
+        <?php include('partials/menu.php'); ?>
+         <!-- main contain section start -->
+         <div class="main-content">
+        <div class="wrapper">
+               <h1>MANAGE CATEGORY</h1>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- main contain section end -->
+        <?php include('partials/footer.php');?>
