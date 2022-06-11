cd5bb8a241740bf519b68b698c375492feb0dff9
Jhilik Maji <jhilik.maji98@gmail.com>
2022-06-11 20:57:33 +0530

    third commit
    Adding dummy table in every admin control page

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -10,8 +10,48 @@
         <!-- main contain section start -->
         <div class="main-content">
         <div class="wrapper">
-               <h1>MANAGE ADMIN</h1>
-                <div class="clearfix"></div>
+               <h1>MANAGE ADMIN</h1><br /><br/>
+                    <!-- button to add admin -->
+                    <a href="#" class="btn-primary">Add Admin</a>
+                    <br/><br/><br/>
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>Sl.No</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+                    <tr>
+                        <td>1.</td>
+                        <td>xyz</td>
+                        <td>xyz name</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2.</td>
+                        <td>xyz</td>
+                        <td>xyz name</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3.</td>
+                        <td>xyz</td>
+                        <td>xyz name</td>
+                        <td>
+                            <a href="#" class="btn-secondary">Update Admin</a>
+                            <a href="#" class="btn-danger">Delete Admin</a>
+                        </td>
+                    </tr>
+                </table>
             </div>
         </div>
         <!-- main contain section end -->
