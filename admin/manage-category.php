b397efb554e9c7966a0278d02355644a06885a30
Shuvasis Manna <shuvasism@gmail.com>
2022-06-12 18:18:26 +0530

    Add Update Delete Category

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -11,53 +11,119 @@
         <div class="wrapper">
                <h1>MANAGE CATEGORY</h1>
                <br/><br/>
-               <?php
+                <?php
                     if(isset($_SESSION['add']))
                     {
                         echo $_SESSION['add'];
                         unset($_SESSION['add']);
-                    }    
+                    }
+
+                    if(isset($_SESSION['remove']))
+                    {
+                        echo $_SESSION['remove'];
+                        unset($_SESSION['remove']);
+                    }
+
+                    if(isset($_SESSION['delete']))
+                    {
+                        echo $_SESSION['delete'];
+                        unset($_SESSION['delete']);
+                    }
+
+                    if(isset($_SESSION['no-category-found']))
+                    {
+                        echo $_SESSION['no-category-found'];
+                        unset($_SESSION['no-category-found']);
+                    }
+
+                    if(isset($_SESSION['update']))
+                    {
+                        echo $_SESSION['update'];
+                        unset($_SESSION['update']);
+                    }
+
+                    if(isset($_SESSION['upload']))
+                    {
+                        echo $_SESSION['upload'];
+                        unset($_SESSION['upload']);
+                    }
+
+                    if(isset($_SESSION['failed-remove']))
+                    {
+                        echo $_SESSION['failed-remove'];
+                        unset($_SESSION['failed-remove']);
+                    }
                 ?>
                 <br><br>
-               <a href="add-category.php" class="btn-primary">Add Category</a>
+               <a href="<?php echo SITEURL; ?>admin/add-category.php" class="btn-primary">Add Category</a>
                <br/><br/><br/>
 
                 <table class="tbl-full">
                     <tr>
                         <th>Sl.No</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Featured</th>
+                        <th>Active</th>
                         <th>Actions</th>
                     </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>xyz</td>
-                        <td>xyz name</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Category</a>
-                            <a href="#" class="btn-danger">Delete Category</a>
-                        </td>
-                    </tr>
 
-                    <tr>
-                        <td>2.</td>
-                        <td>xyz</td>
-                        <td>xyz name</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Category</a>
-                            <a href="#" class="btn-danger">Delete Category</a>
-                        </td>
-                    </tr>
+                    <?php
+                    
+                        $sql = "SELECT * FROM category";
 
-                    <tr>
-                        <td>3.</td>
-                        <td>xyz</td>
-                        <td>xyz name</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Category</a>
-                            <a href="#" class="btn-danger">Delete Category</a>
-                        </td>
-                    </tr>
+                        $res = mysqli_query($conn, $sql);
+
+                        $count = mysqli_num_rows($res);
+
+                        $sl = 1;
+
+                        if($count > 0)
+                        {
+                            while ($row = mysqli_fetch_assoc($res)) {
+                                $id = $row['id'];
+                                $title = $row['title'];
+                                $image_name = $row['image_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+
+                                ?>
+
+                                    <tr>
+                                        <td><?php echo $sl++; ?></td>
+                                        <td><?php echo $title; ?></td>
+                                        <td>
+                                            <?php 
+                                                if($image_name!="") {
+                                                    ?>
+                                                    <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" width="100px">
+                                                    <?php
+                                                } else {
+                                                    echo "<div class='error'>Image not added</div>";
+                                                }
+                                            ?>
+                                        </td>
+                                        <td><?php echo $featured; ?></td>
+                                        <td><?php echo $active; ?></td>
+                                        <td>
+                                            <a href="<?php echo SITEURL; ?>/admin/update-category.php?id=<?php echo $id; ?>" class="btn-secondary">Update Category</a>
+                                            <a href="<?php echo SITEURL; ?>/admin/delete-category.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Category</a>
+                                        </td>
+                                    </tr>
+
+                                <?php
+                            }
+                        }
+                        else
+                        {
+                            ?>
+
+                            <td colspan="6"><div class="error">No Category Added.</div></td>
+                            <?php
+                        }
+                    ?>
+                    
+
                 </table>
                 <div class="clearfix"></div>
             </div>
---
+++ admin/update-category.php
@@ -0,0 +1,167 @@
+<html>
+    <head>
+        <title>Delete Category</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <?php include('partials/menu.php');?>
+
+            <div class="main-content">
+                <div class="wrapper">
+                    <h1>Update Category</h1>
+                    <br><br>
+
+                    <?php
+                        if(isset($_GET['id'])){
+                            // echo "Getting the data";
+                            $id = $_GET['id'];
+
+                            $sql = "SELECT * FROM category WHERE id=$id";
+
+                            $res = mysqli_query($conn, $sql);
+
+                            $count = mysqli_num_rows($res);
+
+                            if($count==1){
+                                $row = mysqli_fetch_assoc($res);
+                                $title = $row['title'];
+                                $current_image = $row['image_name'];
+                                $featured = $row['featured'];
+                                $active = $row['active'];
+                            }else {
+                                $_SESSION['no-category-found'] = "<div class='error'>Category not found</div>";
+                                header('location:'.SITEURL.'admin/manage-category.php');
+                            }
+                        }else{
+                            header('location:'.SITEURL.'admin/manage-category.php');
+                        }
+                    ?>
+
+                        <form action="" method="POST" enctype="multipart/form-data">
+                            <table class="tbl-30">
+                                <tr>
+                                    <td>Title: </td>
+                                    <td>
+                                        <input type="text" name="title" value="<?php echo $title; ?>">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Current Image: </td>
+                                    <td>
+                                        <?php 
+
+                                            if($current_image != "") {
+                                                ?>
+                                                <img src="<?php echo SITEURL; ?>images/category/<?php echo $current_image; ?>" width="100px">
+                                                <?php
+                                            } else {
+                                                echo "<div class='error'>Image not added</div>";
+                                            }
+                                        ?>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>New Image: </td>
+                                    <td>
+                                        <input type="file" name="image">
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Featured: </td>
+                                    <td>
+                                        <input <?php if($featured=="Yes"){echo "checked";} ?> type="radio" name="featured" value="Yes">Yes
+                                        <input <?php if($featured=="No"){echo "checked";} ?> type="radio" name="featured" value="No">No
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Active: </td>
+                                    <td>
+                                        <input <?php if($active=="Yes"){echo "checked";} ?> type="radio" name="active" value="Yes">Yes
+                                        <input <?php if($active=="No"){echo "checked";} ?> type="radio" name="active" value="No">No
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <input type="hidden" name="current_image" value="<?php echo $current_image; ?>">
+                                        <input type="hidden" name="id" value="<?php echo $id; ?>">
+                                        <input type="submit" name="submit" value="Update Categroy" class="btn-secondary">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                        <?php
+
+                            if(isset($_POST['submit'])) {
+                                // echo "clicked";
+
+                                $id = $_POST['id'];
+                                $title = $_POST['title'];
+                                $current_image = $_POST['current_image'];
+                                $featured = $_POST['featured'];
+                                $active = $_POST['active'];
+
+                                if(isset($_FILES['image']['name'])) {
+                                    $image_name = $_FILES['image']['name'];
+
+                                    //A. Upload the image
+                                    if($image_name != "") {
+                                        $ext = end(explode('.', $image_name));
+                                        $image_name = "Food_Category_".rand(000, 999).'.'.$ext;
+                                        $sourece_path = $_FILES['image']['tmp_name'];
+                                        $destination_path = "../images/category/".$image_name;
+
+                                        $upload = move_uploaded_file($sourece_path, $destination_path);
+                                        if($upload==false)
+                                        {
+                                            $_SESSION['upload'] = "<div class='error'>Failed to upload image </div>";
+                                            header('location:'.SITEURL.'admin/manage-category.php');
+                                            die();
+                                        }
+                                    } else {
+                                        $image_name = $current_image;
+                                    }
+                                    //B. Remove current image if availabele 
+                                    if($current_image != "") {
+                                        $remove_path = "../images/category/".$current_image;
+                                        $remove = unlink($remove_path);
+
+                                        //Check weather the image is removed or not
+                                        //If fail to remove then display message and stop the process
+
+                                        if($remove==false) {
+                                            //Fail to remove image
+                                            $_SESSION['failed-remove'] = '<div class="error">Failed to temove current image</div>';
+                                            header('location:'.SITEURL.'admin/manage-category.php');
+                                            die();
+                                        }
+                                    }   
+                                } else {
+                                    $image_name = $current_image;
+                                }
+
+                                $sql2 = "UPDATE category SET
+                                title= '$title',
+                                image_name = '$image_name',
+                                featured= '$featured',
+                                active = '$active'
+                                WHERE id = $id";
+
+                                $res2 = mysqli_query($conn, $sql2);
+
+                                if($res2==true){
+                                    $_SESSION['update'] = "<div class='success'>Category Updated Successfully</div>";
+                                    header('location:'.SITEURL.'admin/manage-category.php');
+                                } else {
+                                    $_SESSION['update'] = "<div class='error'>Category Updated Failed</div>";
+                                    header('location:'.SITEURL.'admin/manage-category.php');
+                                }
+                            }
+                        
+                        ?>
+                </div>
+            </div>
+
+        <?php include('partials/footer.php'); ?>
+    </body>
+</html>
+
